c69f7ac5e16f27673b8771f72cf4322f0aaecdcf
Leva <novikovlev48@gmail.com>
2026-05-02 07:35:06 +0200

    add classify function

--- tools.py
+++ tools.py
@@ -1,25 +1,41 @@
 import numpy as np
 import math
 
-def replace_farthest_neighbour(neighbour, array : list[tuple]):
+def replace_farthest_neighbour(neighbour, nearest_neighbours):
     num = neighbour[1]
 
-    if (array[0][1] > num): 
-        array[0] = neighbour
-        array.sort(key=lambda neighbour: neighbour[1], reverse=True)
-        return array
+    # first neighbour has always the biggest distance
+    if (nearest_neighbours[0][1] > num): 
+        nearest_neighbours[0] = neighbour
+        nearest_neighbours.sort(key=lambda neighbour: neighbour[1], reverse=True)
     
-    return array
+    return nearest_neighbours
     
 
-def find_k_nearest_neighbours(k, coordinates : np.array, data : np.array, labels : np.array):
-    nearest_neighbours = [((0, 999999)) for i in range(k)]
+def find_k_nearest_neighbours(k, target_coordinates : np.array, data : np.array, labels : np.array):
+    # [0] - coordinates
+    # [1] - distance to target
+    # [2] - flower class
+    nearest_neighbours = [[[0, 0, 0, 0], 999999, '-'] for i in range(k)]
     
     for i in range(data.shape[0]):
-
-        curr_len = math.sqrt(sum((data[i] - coordinates) ** 2))
-        curr_neighbour = (i, curr_len)
-
+        flower_name = labels[i]
+        curr_len = math.sqrt(sum((data[i] - target_coordinates) ** 2))
+        curr_neighbour = [coordinataes[i], curr_len, flower_name]
+        
         nearest_neighbours = replace_farthest_neighbour(curr_neighbour, nearest_neighbours)
 
     return nearest_neighbours
+
+
+def classify(k : int, target_coordinates : list[int], data : np.array, labels : np.array):
+    nearest_neighbours = find_k_nearest_neighbours(k, target_coordinates, data, labels)
+    
+    setosa_votes = sum([1 for neighbour in nearest_neighbours if neighbour[2] == 0)
+    versicolor_votes = sum([1 for neighbour in nearest_neighbours if neighbour[2] == 1)
+    virginica_votes = sum([1 for neighbour in nearest_neighbours if neighbour[2] == 2)
+
+    votes = [setosa_votes, versicolor_votes, virginica_votes]
+
+    return votes.index(max(votes))
+    
